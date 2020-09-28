3895b3c4372aca5c4a09231045be79acadf44506
AbdulioKhanio <Akhan17@outlook.com>
2020-09-28 18:09:22 -0500

    added create & show functionality

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,17 +1,28 @@
 class LeadsController < ApplicationController
 
   def index
-    binding.pry
-    @leads = Lead.all
+    @owner = Owner.find_by(id: session["warden.user.owner.key"][0])
+    @leads = Lead.all.reverse
   end
 
   def new
+    @owner = Owner.find_by(id: session["warden.user.owner.key"][0])
+    @lead = Lead.new
   end
 
   def create
+    @lead = Lead.new(lead_params)
+    @lead.owner_id = params["owner_id"].to_i
+
+    if @lead.save 
+      redirect_to leads_path
+    else
+      render :new
+    end 
   end
 
   def show
+    @lead = Lead.find_by(id: params["id"])
   end
 
   def edit
@@ -22,4 +33,10 @@ class LeadsController < ApplicationController
 
   def destroy
   end
+
+  private 
+  def lead_params
+    params.require(:lead).permit(:owner_id, :lead_name, :lead_number, :lead_email, :lead_web, :lead_temp)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :owners
+  
   resources :owners do 
     resources :leads do 
       resources :notes 
