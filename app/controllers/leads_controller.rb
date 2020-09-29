67b35704418f5c915a4405020e3b8565cd026e71
AbdulioKhanio <Akhan17@outlook.com>
2020-09-29 15:26:10 -0500

    delete functionality

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -26,12 +26,24 @@ class LeadsController < ApplicationController
   end
 
   def edit
+    @owner = Owner.find_by(id: session["warden.user.owner.key"][0])
+    @lead = Lead.find_by(id: params["id"])
   end
 
   def update
+    @lead = Lead.find_by(id: params["id"])
+    if @lead.update(lead_params)
+      redirect_to leads_path
+    else 
+      render :edit
+    end 
   end
 
   def destroy
+    int = params["id"].to_i
+    @lead = Lead.find_by(id: int)
+    @lead.destroy
+    redirect_to leads_path
   end
 
   private 
