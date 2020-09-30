c22c7bac698e65dc9e50ae5647001b78d4f861a5
AbdulioKhanio <Akhan17@outlook.com>
2020-09-30 13:46:42 -0500

    heroku init

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,12 @@ class Owner < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-    has_many :leads
-    has_many :notes, through: :leads
+         :recoverable, :rememberable
+  
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :password
+  
+  has_many :leads
+  has_many :notes, through: :leads
 end
