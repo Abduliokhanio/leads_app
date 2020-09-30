65da42479391da8d34fd6d21ecd40fd83d3846eb
AbdulioKhanio <Akhan17@outlook.com>
2020-09-30 14:49:11 -0500

    added name to lead owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_owner!
+    before_action :authenticate_owner!, :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
         session["user_id"] = Owner.where(email: params["owner"]["email"].downcase)[0].id
         leads_path
     end
+
+    def configure_permitted_parameters
+        # Permit the `subscribe_newsletter` parameter along with the other
+        # sign up parameters.
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      end
+
+
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,8 +5,10 @@ class Owner < ApplicationRecord
          :recoverable, :rememberable
   
   validates_presence_of :name
+  validates_uniqueness_of :email
   validates_presence_of :email
   validates_presence_of :password
+  validates_length_of  :password, minimum: 6
   
   has_many :leads
   has_many :notes, through: :leads
